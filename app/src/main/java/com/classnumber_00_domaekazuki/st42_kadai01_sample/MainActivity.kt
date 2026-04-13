aee1d568d43316c1086909fbd82fcf44ebddcf89
haldomae <hal.domae@gmail.com>
2026-04-13 16:33:51 +0900

    first commit

---
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai01_sample/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.classnumber_00_domaekazuki.st42_kadai01_sample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.classnumber_00_domaekazuki.st42_kadai01_sample.ui.theme.ST42_kadai01_sampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ST42_kadai01_sampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ST42_kadai01_sampleTheme {
+        Greeting("Android")
+    }
+}
